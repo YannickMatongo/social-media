b9e4bbd7a2d6b7cb9866d372de3d53a6dac453ba
Yannick Matongo <nay77127@gmail.com>
2023-10-29 18:11:41 +0100

    add home section

---
+++ src/Components/HomeSection/HomeSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomeSection = () => {
+  return (
+    <div className='space-y-5'>
+      <section>
+        <h1 className='py-5 text-xl font-bold opacity-90'>Accueil</h1>
+      </section>
+      <section>
+        
+      </section>
+    </div>
+  )
+}
+
+export default HomeSection
--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,30 +1,88 @@
 import React from "react";
 import { navigationMenu } from "./NavigationMenu";
 import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Avatar, Button, Menu, MenuItem } from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 const Navigation = () => {
-    const navigate=useNavigate();
+  const [anchorEl, setAnchorEl] =
+    React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const navigate = useNavigate();
+  const handleLogout=()=>{
+    console.log("logout")
+    handleClose()
+  }
   return (
     <div className="h-screen sticky top-0">
       <div>
         <div className="py_5">
-            <img src="/logo.png" alt="" className="w-[80px] h-[80px]" />
+          <img src="/logo.png" alt="" className="w-[80px] h-[80px]" />
         </div>
         <div className="space-y-6">
-            {navigationMenu.map((item)=><div className="cursor-pointer flex space-x-3 items-center" onClick={() => item.title === "Profile" ? navigate(`/profile/${5}`) : navigate(item.path)}>
-                {item.icon}
-                <p className="text-xl" >{item.title}</p>
-            </div>)}
+          {navigationMenu.map((item) => (
+            <div
+              className="cursor-pointer flex space-x-3 items-center"
+              onClick={() =>
+                item.title === "Profile"
+                  ? navigate(`/profile/${5}`)
+                  : navigate(item.path)
+              }
+            >
+              {item.icon}
+              <p className="text-xl">{item.title}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="py-10">
+          <Button
+            sx={{
+              width: "100%",
+              borderRadius: "29px",
+              py: "15px",
+              bgColor: "#1e88e5",
+            }}
+            variant="contained"
+          >
+            Tweet
+          </Button>
         </div>
       </div>
-      <div className="py-10">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-3">
+          <Avatar alt="username" />
+          <span>Nom Prenom</span>
+          <span className="opacity-70">@nomutilisateur</span>
+        </div>
+
         <Button
-        sx={{ width:"100%", borderRadius: "29px", py: "15px", bgColor: '#1e88e5' }}
-        variant="contained"
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
         >
-            Tweet
+          <MoreHorizIcon />
         </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleLogout}>Deconnexion</MenuItem>
+        </Menu>
       </div>
     </div>
   );
